7c249c8edb74e518b5e01095029bba952e0cd40b
Michael Krisper <michael.krisper@gmail.com>
2014-11-08 19:46:02 +0100

    added generator for minimal sets

--- part.py
+++ part.py
@@ -11,18 +11,31 @@ __email__ = "michael.krisper@gmail.com"
 __date__ = "2014-11-08"
 
 
+def min_set(base_set):
+    sets = []
+    for s in base_set:
+        current_set = set()
+        for item in s:
+            if all(x not in item and item not in x for x in current_set):
+                current_set.add(item)
+        if current_set not in sets:
+            sets.append(current_set)
+            yield current_set
+
+
 def split(text):
     parts = {text[i:j] for i in range(0, len(text)) for j in range(i + 1, len(text) + 1)}
     parts = sorted(parts, key=lambda x: (len(x), x))
 
     solutions = set()
     powerset = itertools.chain.from_iterable(itertools.combinations(parts, r) for r in range(1, len(parts) + 1))
-    for sub_parts in powerset:
+
+    for sub_parts in min_set(powerset):
         sub_solution = set()
         pos = 0
         while pos < len(text):
             for part in sub_parts:
-                if text.startswith(part, pos) and ((part in sub_solution) or all(x not in part and part not in x for x in sub_solution)):
+                if text.startswith(part, pos):
                     sub_solution.add(part)
                     pos += len(part)
                     break
@@ -31,8 +44,9 @@ def split(text):
         if pos == len(text):
             solution = tuple(sorted(sub_solution))
             if solution not in solutions:
-                yield solution
                 solutions.add(solution)
+                yield solution
+
 
 def main():
     print(set(split("abcd")))
